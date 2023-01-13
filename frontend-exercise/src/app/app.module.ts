72d038152f0d22b9d2cf5c3b584a5c6f23d55ff3
merlinymy <lordhorcrux@gmail.com>
2023-01-12 20:12:15 -0600

    final version-- added styling

--- frontend-exercise/src/app/app.module.ts
+++ frontend-exercise/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { UserCreationComponent } from './components/user-creation/user-creation.component';
 
@@ -16,6 +17,8 @@ import { UserCreationComponent } from './components/user-creation/user-creation.
     FormsModule,
     ReactiveFormsModule, 
     HttpClientModule,
+    MatSelectModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-exercise/src/app/components/user-creation/user-creation.component.ts
+++ frontend-exercise/src/app/components/user-creation/user-creation.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { FormOptions } from 'src/app/model/form-options';
 import { BackendApiService } from 'src/app/services/backend-api.service';
 
@@ -14,17 +14,31 @@ export class UserCreationComponent implements OnInit {
   userCreationForm!: FormGroup;
   occupations!: string[];
   states!: any[];
+  formNotValidMsg!: string;
+  invalidSubmit: boolean = false;
+
+  emailControl!: FormControl;
+  nameControl!: FormControl;
+  passwordControl!: FormControl;
+  occupationControl!: FormControl;
+  stateControl!: FormControl;
 
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private backendApiService: BackendApiService) {}
 
   ngOnInit() {
-    // Create the form
+    // input validation and form validation
+    this.emailControl = new FormControl('', [Validators.email, Validators.required]);
+    this.nameControl = new FormControl('', [Validators.required]);
+    this.passwordControl = new FormControl('', [Validators.required]);
+    this.occupationControl = new FormControl('', [Validators.required]);
+    this.stateControl = new FormControl('', [Validators.required]); 
+    
     this.userCreationForm = this.formBuilder.group({
-      name: ["", Validators.required],
-      email: ["", [Validators.required]],
-      password: ["", Validators.required],
-      occupation: ["", Validators.required],
-      state: ["", Validators.required]
+      name: this.nameControl,
+      email: this.emailControl,
+      password: this.passwordControl,
+      occupation: this.occupationControl,
+      state: this.stateControl
     });
 
     this.getOptions();
@@ -48,6 +62,9 @@ export class UserCreationComponent implements OnInit {
     if (this.userCreationForm.valid) {
       // Send a POST request with the form data to the endpoint
       this.backendApiService.postUserForm(this.userCreationForm.value);
+    } else {
+      this.invalidSubmit = true;
+      this.formNotValidMsg = "There was a problem with your submission. Please review the fields above."
     }
   }
 }
